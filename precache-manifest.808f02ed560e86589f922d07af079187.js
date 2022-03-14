a55180ae303b54d26839afc83a86e4b595442c94
SoftGenics <softgenics.private@gmail.com>
2022-03-14 17:51:40 +0530

    Updates

---
+++ precache-manifest.808f02ed560e86589f922d07af079187.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "af4c71ce874fe0a74f0c",
+    "url": "/Shreeji-Multispecialty-Dental-Clinic/static/css/main.618bfd83.chunk.css"
+  },
+  {
+    "revision": "af4c71ce874fe0a74f0c",
+    "url": "/Shreeji-Multispecialty-Dental-Clinic/static/js/main.207e88ca.chunk.js"
+  },
+  {
+    "revision": "0c34f0fd59a3c8b8daca",
+    "url": "/Shreeji-Multispecialty-Dental-Clinic/static/js/runtime~main.76d9a078.js"
+  },
+  {
+    "revision": "cbc63277d3bd35bc7796",
+    "url": "/Shreeji-Multispecialty-Dental-Clinic/static/css/2.63b94946.chunk.css"
+  },
+  {
+    "revision": "cbc63277d3bd35bc7796",
+    "url": "/Shreeji-Multispecialty-Dental-Clinic/static/js/2.9b579e77.chunk.js"
+  },
+  {
+    "revision": "cb04a4fcc274232b68da9b80ed083b88",
+    "url": "/Shreeji-Multispecialty-Dental-Clinic/static/media/arrow-down-mint.cb04a4fc.svg"
+  },
+  {
+    "revision": "51413f099eab315748ce1b3163fc8a72",
+    "url": "/Shreeji-Multispecialty-Dental-Clinic/index.html"
+  }
+];
